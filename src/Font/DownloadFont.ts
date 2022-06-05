edbd22fddeed2547cc262d0c893a3a46c98b0db2
Skender Gashi <59318537+skendergashi@users.noreply.github.com>
2022-06-05 23:50:34 +0200

    removed download function

--- src/Font/DownloadFont.ts
+++
@@ -1,9 +0,0 @@
-import download from 'download';
-import { FONT_LINK, FOLDER_OF_ZIP } from '../constants';
-import OpenFolder from '../service/OpenFolder';
-
-export async function DownloadFont() {
-    await download(FONT_LINK, process.cwd() + '\\pwsh-config\\Fonts');
-
-    OpenFolder(FOLDER_OF_ZIP);
-}
--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,18 @@
-import * as readline from 'readline';
 import { CopyConfigFile } from './Config/CopyConfigFile';
 import {
     PWSH_PROFILE_PATH_LOCAL,
     PWSH_PROFILE_PATH_PROJECT,
 } from './constants';
-import { DownloadFont } from './Font/DownloadFont';
 
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-});
-
-/**
- * the main function
- * @param downloadFont OPTIONAL: if download is false, it won't download the font
- */
-export async function main(downloadFont?: boolean) {
+export async function main() {
     console.log('\n' + '#####################################' + '\n');
     console.log('Initializing the POSH-Theme...');
-    if (downloadFont || downloadFont === undefined) {
-        await startDownload();
-    }
 
     await CopyConfigFile(PWSH_PROFILE_PATH_PROJECT, PWSH_PROFILE_PATH_LOCAL);
 
     console.log('\n' + 'Config files got copied!');
-    console.log('\n' + 'Now, just restart your computer!' + '\n');
+    console.log('\n' + 'Now, just restart your shell!' + '\n');
     process.exit();
 }
 
-/**
- * Asks the user wether he wants to download the suggested font.
- * The suggested font can be found at /src/constants.ts => FONT_LINK
- */
-async function startDownload() {
-    console.log('Downloading...');
-    await DownloadFont();
-    console.log(
-        '\n' + 'Now, the file explorer should be open. Install the fonts.'
-    );
-}
+main();
