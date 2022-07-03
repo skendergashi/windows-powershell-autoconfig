2c17770783c10e2ebbde3a0228ec2c430f524ff0
Skender Gashi <59318537+skendergashi@users.noreply.github.com>
2022-07-03 20:24:07 +0200

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,7 @@ export async function main() {
 
     await CopyConfigFile(PWSH_PROFILE_PATH_PROJECT, PWSH_PROFILE_PATH_LOCAL);
 
-    console.log('\n' + 'Config files got copied! ');
+    console.log('\n' + 'Config files got copied!');
     console.log('\n' + 'Now, just restart your shell!' + '\n');
     process.exit();
 }
